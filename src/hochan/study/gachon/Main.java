37b2c9076d3a51cdfbbe0aca6d0623d2411ad973
hochany95 <hochany95@gmail.com>
2021-10-25 21:25:53 +0900

    2021-10-25 test2

--- src/hochan/study/gachon/Main.java
+++ src/hochan/study/gachon/Main.java
@@ -3,6 +3,6 @@ package hochan.study.gachon;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello - test");
     }
 }
