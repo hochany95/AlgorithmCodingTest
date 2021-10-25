0dbaaa87bf3bf4612f93a534a84cbf1196fd9fab
hochany95 <hochany95@gmail.com>
2021-10-25 21:53:12 +0900

    2021-10-25 commit

---
+++ src/baekjoon/baekjoon_2789.java
@@ -0,0 +1,24 @@
+package baekjoon;
+import java.util.*;
+public class baekjoon_2789 {
+
+    public static void main(String[] args) {
+        String delString = "CAMBRIDGE";
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        StringBuilder answer = new StringBuilder();
+        boolean appended;
+        for(int i=0; i<input.length(); i++){
+            appended = true;
+            for(int j=0; j<delString.length(); j++){
+                if(input.charAt(i) == delString.charAt(j)){
+                    appended = false;
+                    break;
+                }
+            }
+            if(appended) answer.append(input.charAt(i));
+        }
+        System.out.println(answer.toString());
+
+    }
+}
