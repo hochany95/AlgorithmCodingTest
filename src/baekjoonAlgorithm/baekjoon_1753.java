206d3473006af6273857e0f03aa6410f18f8bf39
hochany95 <hochany95@gmail.com>
2022-01-08 01:37:10 +0900

    2022-01-07 Algorithm

---
+++ src/baekjoonAlgorithm/baekjoon_1753.java
@@ -0,0 +1,89 @@
+package baekjoonAlgorithm;
+
+import etc.Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class baekjoon_1753 {
+
+    static class Node implements Comparable<Node> {
+        private int index, distance;
+
+        public Node(int index, int distance) {
+            this.index = index;
+            this.distance = distance;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public int getDistance() {
+            return distance;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.distance, o.distance);
+        }
+    }
+
+    public static final int INF = (int)1e9;
+    public static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+    public static int[] d;
+
+    public static void dijkstra(int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        d[start] = 0;
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int now = node.getIndex();
+            int distance = node.getDistance();
+
+            if(d[now]<distance) continue;
+
+            for (int i = 0; i < graph.get(now).size(); i++) {
+                int cost = d[now] + graph.get(now).get(i).getDistance();
+                if (d[graph.get(now).get(i).getIndex()] > cost) {
+                    d[graph.get(now).get(i).getIndex()] = cost;
+                    pq.add(new Node(graph.get(now).get(i).getIndex(), cost));
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(br.readLine());
+
+        d = new int[n+1];
+        Arrays.fill(d, INF);
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph.get(a).add(new Node(b, cost));
+        }
+
+        dijkstra(start);
+        for (int i = 1; i <= n; i++) {
+            System.out.println(d[i] != INF ? d[i] : "INF");
+        }
+    }
+}
