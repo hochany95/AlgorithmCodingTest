6e880beb8e6c21017da9cbb8f6052e0e64ebbe70
hochany95 <hochany95@gmail.com>
2022-01-21 00:23:09 +0900

    2022-01-20 Algorithm

--- src/baekjoonAlgorithm/baekjoon_1715.java
+++ src/baekjoonAlgorithm/baekjoon_1715.java
@@ -1,28 +1,29 @@
 package baekjoonAlgorithm;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class baekjoon_1715 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        PriorityQueue<Integer> card = new PriorityQueue<>();
-        for (int i = 0; i < n; i++)
-            card.offer(sc.nextInt());
-
-        if (card.size() < 2) {//카드뭉치가 1개일 경우 0번
-            System.out.print(0);
-            return;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            pq.offer(Integer.parseInt(br.readLine()));
         }
 
-        int total = 0;
-        while(card.size() > 1){
-            int n1 = card.poll();
-            int n2 = card.poll();
-            total += n1+n2;
-            card.offer(n1+n2);
+        int sum = 0;
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            int c = a + b;
+            sum += c;
+            pq.offer(c);
         }
-        System.out.print(total);
+
+        System.out.println(sum);
     }
 }
