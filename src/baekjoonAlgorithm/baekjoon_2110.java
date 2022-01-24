1a63415924b946ad0e83625f5566621391ba089c
hochany95 <hochany95@gmail.com>
2022-01-24 15:18:31 +0900

    2022-01-24 Algorithm

--- src/baekjoonAlgorithm/baekjoon_2110.java
+++ src/baekjoonAlgorithm/baekjoon_2110.java
@@ -1,39 +1,49 @@
 package baekjoonAlgorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 public class baekjoon_2110 {
-    public static int getCount(int[] arr, int d){
-        int count = 1;
-        int now = arr[0];
-        for(int i=1; i<arr.length; i++){
-            if(arr[i] - now >= d){
-                now = arr[i];
-                count++;
-            }
-        }
-        return count;
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int c = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++)
-            arr[i] = sc.nextInt();
-        Arrays.sort(arr);
-        int start = 0;
-        int end = arr[n-1];
-        int ans = -1;
-        while(start<end){
+    public static int findCount(int[] arr, int target) {
+        int start = 1;//최소 간격
+        int end = arr[arr.length-1] - arr[0];//범위 최대
+        int count;
+        int ans = 1;
+        while (start <= end) {
             int mid = (start + end) / 2;
+            int now = arr[0] + mid;
+            int idx = 1;
 
-            if(getCount(arr, mid) >= c){
+            count = 1;
+            while (idx < arr.length) {
+                if (arr[idx] >= now) {
+                    count++;
+                    now = arr[idx] + mid;
+                }
+                idx++;
+            }
+            if (count >= target) {//
                 ans = mid;
                 start = mid+1;
+
+            } else {
+                end = mid -1;
             }
-            else
-                end = mid;
         }
-        System.out.println(ans);
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        System.out.println(findCount(arr, c));
     }
 }
