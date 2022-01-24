ea5f8ab30f8be3de6c5099fff001bfe718c8a1d7
hochany95 <hochany95@gmail.com>
2022-01-24 18:40:10 +0900

    2022-01-24 Algorithm

---
+++ src/baekjoonAlgorithm/baekjoon_10815.java
@@ -0,0 +1,47 @@
+package baekjoonAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baekjoon_10815 {
+
+    public static boolean findNum(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return true;
+            }
+
+            if(arr[mid] > target){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            sb.append(findNum(arr, Integer.parseInt(st.nextToken()))? "1 ": "0 ");
+        }
+        System.out.println(sb.toString());
+    }
+}
