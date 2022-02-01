fa681c653aa46a3b13c0df84ab188e12d460b8f5
hochany95 <hochany95@gmail.com>
2022-02-01 22:52:22 +0900

    2022-02-01 Algorithm

---
+++ src/baekjoonAlgorithm/baekjoon_2458.java
@@ -0,0 +1,52 @@
+package baekjoonAlgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class baekjoon_2458 {
+
+    public static final int INF = (int)1e9;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] graph = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(graph[i], INF);
+            graph[i][i] = 0;
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = 1;
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            boolean is = true;
+            for (int j = 1; j <= n; j++) {
+                if(graph[i][j] == INF && graph[j][i] == INF){
+                    is = false;
+                    break;
+                }
+            }
+            if(is)
+                count++;
+
+        }
+        System.out.println(count);
+    }
+}
